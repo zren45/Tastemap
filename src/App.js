5401b1165d53c5efe273ecdcee41c574bdd77b08
zren45 <zhixiangren@gatech.edu>
2022-01-08 18:45:10 -0600

    basic layout

--- src/App.js
+++ src/App.js
@@ -10,7 +10,14 @@ const App = () => {
     <div>
       <CssBaseline />
       <Header />
-      <Grid container spacing={3} style={{ width: "100%" }}></Grid>
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List></List>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Map></Map>
+        </Grid>
+      </Grid>
     </div>
   );
 };
